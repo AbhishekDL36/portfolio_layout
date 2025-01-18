8396994a596b7d25187fc45b2bc5b92640a4dde4
AbhishekDL36 <abhishekofficials12@gmail.com>
2025-01-18 12:27:58 +0530

    css updated

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,12 +4,12 @@ function Navbar() {
   return (
     <header className="bg-white shadow sticky top-0">
       <div className="container mx-auto flex items-center justify-between py-5 px-7">
-        <h1 className="text-2xl font-bold name">Your Name</h1>
+        <h1 className="text-2xl font-bold name">ABHISHEK</h1>
         <nav className="space-x-4">
-          <a href="#about" className="hover:text-blue-500">About</a>
-          <a href="#projects" className="hover:text-blue-500">Projects</a>
-          <a href="#skills" className="hover:text-blue-500">Skills</a>
-          <a href="#contact" className="hover:text-blue-500">Contact</a>
+          <a href="#about" className="hover:text-blue-500 text-[22px]">About</a>
+          <a href="#projects" className="hover:text-blue-500 text-[22px]">Projects</a>
+          <a href="#skills" className="hover:text-blue-500 text-[22px]">Skills</a>
+          <a href="#contact" className="hover:text-blue-500 text-[22px]">Contact</a>
         </nav>
       </div>
     </header>
